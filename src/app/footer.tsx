26c54c545d1d5e1a472352335b19112e77f074ec
Gerrybp12 <gerry.bimaputra@gmail.com>
2024-09-16 11:55:52 +0700

    Footer responsive update

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -7,15 +7,15 @@ const logoStyle = {color: "white", fontSize: "1.5em"};
 
 const Footer = () => {
     return (
-        <div className="flex flex-col justify-center text-center items-center min-h-[20rem] bg-black bg-opacity-50">
-            <h1 className="font-poppins font-medium text-7xl text-white opacity-70">Contact Me</h1>
+        <div className="flex flex-col justify-center text-center items-center bg-black bg-opacity-50">
+            <h1 className="mt-10 font-poppins font-medium text-[10vw] md:text-7xl text-white opacity-50">Contact Me</h1>
             <h1 className="font-poppins font-light text-[1rem] text-white opacity-70 mb-7">I’d love to hear from you. Here’s how you can get in touch with me:</h1>
-            <div className='min-w-[30rem] justify-between flex flex-row'>
-            <div>
+            <div className='mb-10 justify-between flex flex-row bg-black'>
+            <div className='mx-10'>
             <div className='flex flex-row my-2'>
                 <CiMail style={logoStyle} />
                 <a
-                className='text-white mx-2 font-poppins text-[1rem] opacity-70 hover:text-[#FC7E00] hover:opacity-100'
+                className='text-white mx-2 font-poppins md:mt-0 mt-1     text-[2vw] md:text-[1rem] opacity-70 hover:text-[#FC7E00] hover:opacity-100'
                 href='mailto:gerry.bimaputra@gmail.com'
                 >gerry.bimaputra@gmail.com</a>
             </div>
@@ -34,7 +34,7 @@ const Footer = () => {
                 >GitHub</a>
             </div>
             </div>
-            <div>
+            <div className='mx-10'>
             <div className='flex flex-row my-2'>
                 <FaInstagram style={logoStyle} />
                 <a
